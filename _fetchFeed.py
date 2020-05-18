b27393b54611102ff42362abde41d4f6d2fcab57
Brian Ballsun-Stanton <brian.ballsun-stanton@mq.edu.au>
2020-05-18 18:10:00 +1000

    removing direnv, gitignore

--- _fetchFeed.py
+++ _fetchFeed.py
@@ -81,7 +81,7 @@ tags:
 </iframe>
 </div>
 
-<script type="text/javascript" src="/js/qr/qrcode.js"></script>
+<script type="text/javascript" src="{{site.baseurl}}/js/qr/qrcode.js"></script>
 <script type="text/javascript">
 new QRCode(document.getElementById("qrcode"), "{}");
 </script>
